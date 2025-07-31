945b26544bd6d873cf3b689949d6f5d3eeb53c3c
Vicky Kumar <vk2388275@gmail.com>
2025-07-31 17:34:10 +0530

    Commit

--- Frontend/src/pages/GetQuote.jsx
+++ Frontend/src/pages/GetQuote.jsx
@@ -29,7 +29,7 @@ const GetQuote = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/quotes", {
+      const response = await fetch("https://trade-biz-worldwide-1.onrender.com/api/quotes", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
