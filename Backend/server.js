5b25b66c1ef2555c3b5d89f014d5777899a3fc12
Vicky Kumar <vk2388275@gmail.com>
2025-07-31 16:54:00 +0530

    Backend

--- Backend/server.js
+++ Backend/server.js
@@ -22,3 +22,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
